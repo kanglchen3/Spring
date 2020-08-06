cbed853a6101c4d91252c6ac90254b8d238ec2cc
hasee <460195106@qq.com>
2020-08-07 01:25:38 +0800

    8.6 at home

---
+++ itheima_spring_jdbc/src/test/java/com/itheima/test/JdbcTemplateTest.java
@@ -0,0 +1,13 @@
+package com.itheima.test;
+
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class JdbcTemplateTest {
+
+    @Test
+    //测试jdbcTemplate开发步骤
+    public void test1(){
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+    };
+}
