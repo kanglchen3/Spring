52a7562ad891235952bc9004d0ee14218673211f
hasee <460195106@qq.com>
2020-09-08 22:58:45 +0800

    9.8 at home

---
+++ itheima_spring_tx/src/test/java/com/itheima/dao/impl/AccountDaoImpl.java
@@ -0,0 +1,29 @@
+package com.itheima.dao.impl;
+
+import com.itheima.dao.AccountDao;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration("classpath:applicationContext.xml")
+public class AccountDaoImpl implements AccountDao {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Test
+    @Override
+    public void out(String outMan, double money){
+        jdbcTemplate.update("update account set money=money-? where name=?", money, outMan);
+    }
+
+    @Override
+    public void in(String inMan, double money){
+        jdbcTemplate.update("update account set money=money+? where name=?", money, inMan);
+    }
+}
