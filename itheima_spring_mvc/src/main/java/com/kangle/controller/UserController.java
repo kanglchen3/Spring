8424c5ad4dc02b24013bc28cecfcaeee14b2dd3c
hasee <460195106@qq.com>
2020-09-15 00:03:40 +0800

    9.14 at home

--- itheima_spring_mvc/src/main/java/com/kangle/controller/UserController.java
+++ itheima_spring_mvc/src/main/java/com/kangle/controller/UserController.java
@@ -2,13 +2,16 @@ package com.kangle.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping("/tt")
+    @RequestMapping(value="/tt", method= RequestMethod.GET, params = {"username"})
     public String save(){
         System.out.println("Controller save running...");
-        return "success.jsp";
+//        return "success";   //能用prefix suffix
+        return "redirect:/success.jsp";
     }
 }
