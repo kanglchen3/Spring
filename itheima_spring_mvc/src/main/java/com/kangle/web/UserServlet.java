7a5e58e7eb44c64561c31f039c5259d565caae77
Kangle Chen <kangle.chen@sap.com>
2020-09-14 18:25:14 +0800

    9.14 at office

--- itheima_spring_mvc/src/main/java/com/kangle/web/UserServlet.java
+++ itheima_spring_mvc/src/main/java/com/kangle/web/UserServlet.java
@@ -1,9 +1,10 @@
 package com.kangle.web;
 
-import com.kangle.listener.WebApplicationContextUtils;
 import com.kangle.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -18,8 +19,17 @@ public class UserServlet extends HttpServlet {
 //        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
 //        ServletContext servletContext = req.getServletContext();
         ServletContext servletContext = this.getServletContext();
+//        去拿servlet context下的 app 这个attribute
 //        ApplicationContext app = (ApplicationContext) servletContext.getAttribute("app");
-        ApplicationContext app = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+//        通过utils 的静态方法去拿 app，避免"app"的耦合性
+//        ApplicationContext app = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+
+
+
+//      spring-web提供的utils里面自带的监听器
+        WebApplicationContext app = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+
+
         UserService userService = app.getBean(UserService.class);
         userService.save();
     }
---
+++ itheima_spring_mvc/web/success.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: I346867
+  Date: 9/14/2020
+  Time: 5:52 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>Success!</h1>
+</body>
+</html>
